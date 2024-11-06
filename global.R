2613327fec892189cad86af5af39fbf494dadbbf
Isaac Shon <isaacshon2002@gmail.com>
2024-11-06 16:50:05 -0500

    first commit

---
+++ global.R
@@ -0,0 +1,69 @@
+#install.packages("httr")
+#install.packages("jsonlite")
+#install.packages("shiny")
+#install.packages("DT")
+#install.packages("forecast")
+library(httr)
+library(jsonlite)
+library(shiny)
+library(bslib)
+library(DT)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(forecast)
+
+#-------------------------------------------------------------------------------#
+# Retrieve Incident-Level Data from NYC Open Data API
+url <- "https://data.cityofnewyork.us/resource/833y-fsy8.json?$limit=50000"
+response <- GET(url)
+
+# Check if the request was successful
+if (status_code(response) == 200) {
+  df <- fromJSON(content(response, "text"))
+  print(paste("Successfully retrieved data."))  # Print or process the data as needed
+} else {
+  print(paste("Failed to retrieve data. Status code:", status_code(response)))
+}
+#-------------------------------------------------------------------------------#
+# Some Data Cleaning:
+
+df$occur_date <- str_sub(df$occur_date, 1, 10)
+df$occur_date <- as.Date(df$occur_date, format = "%Y-%m-%d")
+#-------------------------------------------------------------------------------#
+# Number of incidents over time:
+
+incident_count <- df %>% mutate(month = floor_date(occur_date, "month")) %>% 
+  group_by(month) %>% 
+  summarise(incidents = n() )
+
+# Convert to time series object
+ts_data <- ts(incident_count$incidents, frequency = 12, start = c(2006, 1))
+
+# Decompose the time series
+decomp <- stl(ts_data, s.window = "periodic")
+
+#-------------------------------------------------------------------------------#
+# Breakdown of Incident Location Type:
+
+location_type <- df %>% filter(loc_classfctn_desc != "(null)") %>% 
+  group_by(loc_classfctn_desc) %>% 
+  summarise(incidents = n())
+
+#-------------------------------------------------------------------------------#
+# Breakdown of Victim Age Group:
+
+victim_age_group <- df %>% 
+  mutate(vic_age_group = ifelse(vic_age_group == "(null)", "UNKNOWN", vic_age_group)) %>% 
+  filter(vic_age_group != "1022") %>% 
+  group_by(vic_age_group) %>% 
+  summarise(incidents = n())
+
+#-------------------------------------------------------------------------------#
+
+
+
+
+
+
---
+++ server.R
@@ -0,0 +1,36 @@
+function(input, output, session){
+  # Table of Loaded Data:
+  output$data_table <- DT::renderDataTable(df, options = list(scrollX = TRUE))
+  
+  # Line plot
+  output$line_plot <- renderPlotly({
+    autoplot(decomp) +
+      theme_minimal() +
+      labs(title = "Decomposition of Monthly Shooting Incidents", x = "Date")
+  })
+  
+  # Barplots of Data:
+  output$bar1 <- renderPlotly({
+    ggplot(location_type, aes(x = reorder(loc_classfctn_desc, -incidents), y = incidents)) +
+      geom_bar(stat = "identity", fill = "steelblue") +
+      labs(title = "Incident Count by Location Classification",
+           x = "Location Classification",
+           y = "Number of Incidents") +
+      theme_minimal()
+  })
+  
+  output$bar2 <- renderPlotly({
+    ggplot(victim_age_group, aes(x = vic_age_group, y = incidents)) +
+      geom_bar(stat = "identity", fill = "steelblue") +
+      labs(title = "Incident Count by Victim Age Group",
+           x = "Victim Age Group",
+           y = "Number of Incidents") +
+      theme_minimal()
+  })
+    
+  # Histogram of Data:
+  output$histogram <- renderPlotly({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
